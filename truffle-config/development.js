8e6ba41608de4adba01c64cd3ca984265c78ec78
brigide <brigidegd@gmail.com>
2022-08-14 01:18:42 -0300

    started setting up docker env

---
+++ truffle-config/development.js
@@ -0,0 +1,24 @@
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+    networks: {
+        development: {
+            host: 'higia.blockchain.ganache',
+            port: 8545,
+            network_id: '*'
+        },
+    },
+    contracts_directory: '/home/app/src/contracts/',
+    contracts_build_directory: '/home/app/src/abis/',
+    compilers: {
+        solc: {
+            version: '^0.8.2',
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    }
+
+}
