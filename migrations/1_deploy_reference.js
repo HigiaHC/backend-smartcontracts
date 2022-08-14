0813380b9b4888c8712084fdf8a75f78642a40cb
brigide <brigidegd@gmail.com>
2022-08-14 14:18:33 -0300

    migration setup

---
+++ migrations/1_deploy_reference.js
@@ -0,0 +1,5 @@
+const Reference = artifacts.require('Reference');
+
+module.exports = function (deployer) {
+    deployer.deploy(Reference);
+}
--- src/contracts/reference.sol
+++ src/contracts/reference.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.5.0 <0.9.0;
 
-contract Reference {}
+contract Reference {
+    struct User {
+        string name;
+        string password;
+    }
+
+    mapping(address => User) public users;
+    mapping(address => string) public references;
+    address public manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function sum(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+}
