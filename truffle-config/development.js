ebb917ad20ce466a436fbcb225bbb569bffedb97
brigide <brigidegd@gmail.com>
2022-08-14 14:18:33 -0300

    migration setup

--- truffle-config/development.js
+++ truffle-config/development.js
@@ -4,13 +4,13 @@ require('babel-polyfill');
 module.exports = {
     networks: {
         development: {
-            host: 'higia.blockchain.ganache',
+            host: 'higia-blockchain-ganache',
             port: 8545,
             network_id: '*'
         },
     },
-    contracts_directory: '/home/app/src/contracts/',
-    contracts_build_directory: '/home/app/src/abis/',
+    contracts_directory: '/home/app/contracts/',
+    contracts_build_directory: '/home/app/abis/',
     compilers: {
         solc: {
             version: '^0.8.2',
